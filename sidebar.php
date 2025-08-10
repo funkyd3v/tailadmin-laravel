b58e78cd6abcdb9eade1b2e8de464fddc4630be4
funkyd3v <95173888+funkyd3v@users.noreply.github.com>
2025-08-10 23:23:13 +0600

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -4,3 +4,43 @@
 * This is the sidebar config file.
 * Add all sidebar menu items here
 */
+
+<?php
+
+// return [
+//     [
+//         'title' => 'Dashboard',
+//         'icon'  => 'fa fa-dashboard',
+//         'url'   => '/admin/dashboard',
+//         'order' => 1,
+//         'children' => [] // No submenus
+//     ],
+//     [
+//         'title' => 'Users',
+//         'icon'  => 'fa fa-users',
+//         'url'   => '#', // Parent menu has no direct link
+//         'order' => 2,
+//         'children' => [
+//             [
+//                 'title' => 'All Users',
+//                 'icon'  => 'fa fa-list',
+//                 'url'   => '/admin/users',
+//                 'order' => 1,
+//             ],
+//             [
+//                 'title' => 'Create User',
+//                 'icon'  => 'fa fa-plus',
+//                 'url'   => '/admin/users/create',
+//                 'order' => 2,
+//             ]
+//         ]
+//     ],
+//     [
+//         'title' => 'Settings',
+//         'icon'  => 'fa fa-cogs',
+//         'url'   => '/admin/settings',
+//         'order' => 3,
+//         'children' => []
+//     ]
+// ];
+
